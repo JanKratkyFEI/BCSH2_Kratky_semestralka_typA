510318a684422ed4f7cbc7ab2cfacc9255ce9fde
JanKratkyFEI <85405339+Honzajknize@users.noreply.github.com>
2024-10-09 17:12:27 +0200

    Add project files.

---
+++ BCSH2_Kratky_semestralka_typA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BCSH2_Kratky_semestralka_typA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
